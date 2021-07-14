8fdacbd1472dd3aa782cd38f027341d8a9587919
Holger Müller <holger.mueller@gipmbh.de>
2021-07-14 12:30:20 +0200

    apply new maven reactor structure

--- src/test/java/de/gipmbh/kidicap/workflow/camunda/unit/agileprocess/ProcessConstants.java
+++
@@ -1,9 +0,0 @@
-package de.gipmbh.kidicap.workflow.camunda.unit.agileprocess;
-
-public class ProcessConstants {
-
-    public static final String PROCESS_DEFINITION_KEY = "server"; // BPMN Process ID
-
-    public static final String PROCESS_VAR_CONTEXT = "context";
-
-}
